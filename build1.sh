c28d5baf8598cf13b3f89b9bd7b7dc0ae9920e54
name <email>
2022-11-05 21:52:05 +0100

    5

--- build1.sh
+++ build1.sh
@@ -18,11 +18,26 @@ LIBS_DIR="${DIR}/.build/dylibs/${PLATFORM_NAME}"
 
 mkdir -p "${LIBS_DIR}"
 
-# if [ ${PLATFORM_NAME} = ${IPHONESIMULATOR_PLATFORM} ]; then
-#     markLibsForSimulator
-# else
-#     markLibsForIos
-# fi
+
+markLibForIos() {
+    perl -pi -e 's/\x32\0\0\0(\x20|\x28)\0\0\0\x01\0\0\0\0\0(\x0b|\x0c)\0\0(\0|\x01|\x02|\x03|\x04)(\x0b|\x0c)\0/\x32\0\0\0\1\0\0\0\x02\0\0\0\0\0\2\0\0\3\4\0/g' ${1}
+}
+
+markLibForSimulator() {
+    perl -pi -e 's/\x32\0\0\0(\x20|\x28)\0\0\0\x01\0\0\0\0\0(\x0b|\x0c)\0\0(\0|\x01|\x02|\x03|\x04)(\x0b|\x0c)\0/\x32\0\0\0\1\0\0\0\x07\0\0\0\0\0\2\0\0\3\4\0/g' ${1}
+}
+
+markLib() {
+    if [ ${PLATFORM_NAME} = ${IPHONESIMULATOR_PLATFORM} ]; then
+        # markLibForSimulator "${1}"
+    else
+        # markLibForIos "${1}"
+    fi
+}
+
+signLib() {
+    # codesign -f -s 949CA008AA70C44D456B5C63DFF47B488897AF14 "${1}"
+}
 
 
 for MODULE in $(ls -1 "${DIR}/modules"); do
@@ -30,16 +45,13 @@ for MODULE in $(ls -1 "${DIR}/modules"); do
 done
 
 
-for MODULE in ${MODULES}
-do
-    # echo "!!!!module: ${DIR}/${MODULE}"
+for MODULE in ${MODULES}; do
     EXISTING_LIB_PATH="$(find ${LIBS_DIR} | grep "${MODULE}-" | head -n 1)"
 
     if [ ! -f "${EXISTING_LIB_PATH}" ]; then
         CHANGED_MODULES+=(${MODULE})
         continue
     fi
-
 
     for SRC_FILE in $(find "${DIR}/modules/${MODULE}/src" | egrep "\.hs$|\.lhs$|\.c$"); do
         if [ "${SRC_FILE}" -nt "${EXISTING_LIB_PATH}" ]; then
@@ -74,16 +86,168 @@ perl -i -pe "BEGIN{undef $/;} s/(build-depends:\n)(.*)(^(?!    $))/\1 ${ROOT_DEP
 (cd "${DIR}/root" && env -i HOME="$HOME" PATH="$PATH" USER="$USER" cabal build --ghc-options="-threaded -O2 +RTS -A64m -AL128m -qn8 -RTS -optc -Wno-nullability-completeness -optc -Wno-expansion-to-defined -optc -Wno-availability -optc -Wno-int-conversion")
 
 
-# for MODULE in ${CHANGED_MODULES}; do
-#     for f in ${DIR}/root/dist-newstyle/build/*/*/${MODULE}*/build/*.dylib; do
-#         cp "$f" "${DIR}/1/"
-#     done
-#     # cp "${DIR}/root/dist-newstyle/build/*/*/${MODULE}*/build/*.dylib" "${DIR}/"
-#     # cp "${DIR}/root/dist-newstyle/build/*/*" "${DIR}/"
+for MODULE in ${CHANGED_MODULES}; do
+    for LIB in ${DIR}/root/dist-newstyle/build/*/*/${MODULE}*/build/*.dylib; do
+        LIB_NAME=$(basename ${LIB})
+        UPDATED_LIB="${LIBS_DIR}/${LIB_NAME}"
+        cp "${LIB}" "${UPDATED_LIB}"
+        (markLib "${UPDATED_LIB}" && signLib "${UPDATED_LIB}") || rm "${UPDATED_LIB}"
+    done
+done
 
-#     # ditto ${MODULE_DIR}/**/
 
+declare -A existingLibs
+
+for EXISTING_LIB_FILE_NAME in $(ls "${LIBS_DIR}"); do
+    existingLibs[${EXISTING_LIB_FILE_NAME}]=1
+done
+
+
+CABAL_DIR=$(zsh current_cabal_store_dir.sh)
+GHCUP_DIR=$(zsh current_ghc_dir.sh)
+
+CABAL_LIBS=$(find ${CABAL_DIR} | grep 'lib.*.dylib')
+GHC_LIBS=$(find ${GHCUP_DIR} | grep 'lib.*.dylib')
+HASKELL_LIBS="${CABAL_LIBS}\n${GHC_LIBS}"
+
+declare -A allDeps
+
+addRtsToDeps() {
+    RTS_LIB_FILE_NAME=$(zsh find_rts_lib.sh)
+    RTS_LIB_FILE=$(echo ${GHC_LIBS} | grep "${RTS_LIB_FILE_NAME}")
+    allDeps[${RTS_LIB_FILE_NAME}]=${RTS_LIB_FILE}
+}
+
+for key val in "${(@kv)existingLibs}"; do
+    if [[ $key == libHSrts* ]]; then
+        RTS_ALREADY_COPIED=1
+    fi
+done
+
+if [ -z "${RTS_ALREADY_COPIED}" ]; then
+    echo "Add rts to deps"
+    addRtsToDeps
+fi
+
+
+findFile() {
+    echo ${HASKELL_LIBS} | grep $1$ | head -n 1
+}
+
+collectDeps() {
+    LIB_FILE_NAME=${1}
+    if [ -n "${allDeps[$LIB_FILE_NAME]}" ]; then
+        return
+    fi
+    LIB_FILE_PATH=${2}
+
+    allDeps[${LIB_FILE_NAME}]=${LIB_FILE_PATH}
+
+    otool -L ${LIB_FILE_PATH} | grep -v "\-inplace\-" | grep -o 'libHS.*.dylib' | while read LIB_FILE_NAME; do
+        collectDeps ${LIB_FILE_NAME} $(findFile ${LIB_FILE_NAME})
+    done
+}
+
+for MODULE in ${CHANGED_MODULES}; do
+    for BUILT_LIB in ${DIR}/root/dist-newstyle/build/*/*/${MODULE}*/build/*.dylib; do
+        LIB_FILE_NAME=$(basename ${BUILT_LIB})
+        collectDeps ${LIB_FILE_NAME} ${BUILT_LIB}
+    done
+done
+
+
+for key val in "${(@kv)allDeps}"; do
+    echo "allDeps: ${key} -> ${val}"
+done
+
+# for key val in "${(@kv)immediateToLink}"; do
+#     echo "immediateToLink: ${key} -> ${val}"
 # done
+
+for LIB_FILE_NAME LIB_FILE_PATH in "${(@kv)allDeps}"; do
+    if [ -z "${existingLibs[$LIB_FILE_NAME]}" ]; then
+        UPDATED_LIB="${LIBS_DIR}/${LIB_FILE_NAME}"
+        cp "${LIB_FILE_PATH}" "${UPDATED_LIB}"
+        (markLib "${UPDATED_LIB}" && signLib "${UPDATED_LIB}") || rm "${UPDATED_LIB}"
+    fi
+done
+
+
+declare -A immediateToLink
+
+collectImmediateToLink() {
+    LIB_FILE_NAME=${1}
+    if [ -n "${immediateToLink[$LIB_FILE_NAME]}" ]; then
+        return
+    fi
+    LIB_FILE_PATH=${2}
+
+    immediateToLink[${LIB_FILE_NAME}]=${LIB_FILE_PATH}
+     
+    otool -L ${LIB_FILE_PATH} | grep -o "libHS.*dylib" | while read LIB_FILE_NAME; do
+        collectImmediateToLink ${LIB_FILE_NAME} "${LIBS_DIR}/${LIB_FILE_NAME}"
+    done
+}
+
+# for key val in "${(@kv)existingLibs}"; do
+#     if [[ $key == libHSrts* ]]; then
+#         RTS_ALREADY_COPIED=1
+#     fi
+# done
+
+# find
+# $(ls -1 "${LIBS_DIR}")
+# (cd ${LIBS_DIR} && )
+
+MAIN_LIB_PATH=$(find ${LIBS_DIR} | grep "libHS${MAIN_MODULE_NAME}-" | head -n 1)
+
+collectImmediateToLink $(basename ${MAIN_LIB_PATH}) ${MAIN_LIB_PATH}
+
+
+for key val in "${(@kv)immediateToLink}"; do
+    echo "${key} -> ${val}"
+done
+
+# codesign -f -s 949CA008AA70C44D456B5C63DFF47B488897AF14 ${LIBS_DIR}/*
+
+# LINKED_TO_DIR=$(readlink -f "${DIR}/dylibs")
+
+# if [ "${LINKED_TO_DIR}" != "${LIBS_DIR}" ]; then
+#     echo "Switch to ${LIBS_DIR}"
+#     rm -f "${DIR}/dylibs"
+#     ln -s "${LIBS_DIR}" "${DIR}/dylibs"
+# fi
+
+# echo "allDeps2: ${allDeps}"
+
+
+# updateLibs() {
+    # CABAL_DIR=$(zsh current_cabal_store_dir.sh)
+    # GHCUP_DIR=$(zsh current_ghc_dir.sh)
+
+    # CABAL_LIBS=$(find ${CABAL_DIR} | grep 'lib.*.dylib')
+    # GHC_LIBS=$(find ${GHCUP_DIR} | grep 'lib.*.dylib')
+    # HASKELL_LIBS="${CABAL_LIBS}\n${GHC_LIBS}"
+
+    # addRts
+    # collectDeps ${LIB_FILE_NAME} ${BUILT_LIB}
+    
+    # mkdir -p ${LIB_DIR}
+
+    # (rm ${LIB_DIR}/* || true) 2> /dev/null
+    # cp ${allDeps} ${LIB_DIR}
+
+    # if [ ${PLATFORM_NAME} = ${IPHONESIMULATOR_PLATFORM} ]; then
+    #     markLibsForSimulator
+    # else
+    #     markLibsForIos
+    # fi
+
+    # signLibs
+# }
+
+# LIBS_DIR="${DIR}/.build/dylibs/${PLATFORM_NAME}"
+
 
 
 # MODULES=$(cd ${MODULE_DIR} && find src | grep .hs$ | sed "s/src\//    /" | sed "s/.hs//" | sed "s/\//./" | sort)
@@ -109,7 +273,7 @@ perl -i -pe "BEGIN{undef $/;} s/(build-depends:\n)(.*)(^(?!    $))/\1 ${ROOT_DEP
 #  1) (many) add sources to cabal files
 #  2) (one/many) add modules to cabal.project
 #  3) (one) cabal build
-#  4) (many) copy&patch only changed dylibs
+#  4) (many) copy&patch&sign only changed dylibs
 #  5) (one) create .filesToLink (add main + rts)
 #  
 
